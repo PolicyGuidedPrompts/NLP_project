8afe5b9e909e7439dd0ade1e77e2590cda4cb42c
danielmor-starkware <daniel.m@starkware.co>
2023-08-05 17:14:51 +0300

    added Agent, looks good, need to work on max len of episode to not exceed from longest prompt possible

--- main.py
+++ main.py
@@ -1,47 +1,29 @@
-# import torch
+import torch
 # import wandb
 # from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, GPT3LMHeadModel
 # from torch.distributions import Categorical
 from datasets import load_dataset
-from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from utils.utils import load_or_download_model
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
 
 # 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 (llama 2 read acces token)
-import os
 
 # Dataset path
 DATA_PATH = "path_to_your_dataset"
 
+
 # Initialize wandb
 # wandb.init(project="NLP-RL")
 
-from transformers import AutoTokenizer, GPT2LMHeadModel, AutoModelForCausalLM
-
 
 class Environment:
-    def __init__(
-        self, prompt, correct_answer, gpt_model_name, special_action, training_dataset
-    ):
-        model_dir = os.path.abspath("./saved_model")
-
-        # Try loading the model from a local directory. If it doesn't exist, download it and save it locally
-        if (
-            os.path.exists(os.path.join(model_dir, "config.json"))
-            and os.path.exists(os.path.join(model_dir, "pytorch_model.bin"))
-            and os.path.exists(os.path.join(model_dir, "tokenizer_config.json"))
-            and os.path.exists(os.path.join(model_dir, "vocab.json"))
-        ):
-            self.tokenizer = AutoTokenizer.from_pretrained(model_dir)
-            self.model = GPT2LMHeadModel.from_pretrained(model_dir)
-        else:
-            self.tokenizer = AutoTokenizer.from_pretrained(gpt_model_name)
-            self.model = GPT2LMHeadModel.from_pretrained(gpt_model_name)
-            self.tokenizer.save_pretrained(model_dir)
-            self.model.save_pretrained(model_dir)
-
-        self.original_question = prompt
-        self.correct_answer = correct_answer
-        self.special_action = special_action
+    def __init__(self, training_dataset, special_action=0):
         self.training_dataset = training_dataset  # A list of question+answer pairs
+        self.encoder_tokenizer, self.encoder_model = load_or_download_model()
+        self.special_action = special_action
         self.reset()
 
     def step(self, action):
@@ -53,111 +35,118 @@ class Environment:
             reward = self.evaluate_prompt()
         else:
             # Concatenate the selected question and answer to the current prompt
-            question,answer = self.training_dataset.iloc[action]
-            self.current_prompt = f"{question}\n{answer}\n{self.current_prompt}"
+            sampled_question, sampled_answer = self.training_dataset.iloc[action]
+            self.question = f"{sampled_question}\n{sampled_answer}\n{self.question}"
 
-        return self.current_prompt, reward, done
+        return self.encode_question(self.question), reward, done
 
     def reset(self):
-        self.current_prompt = self.original_question
+        self.question, self.answer = self.training_dataset.sample(1).iloc[0]
+        return self.encode_question(self.question)
 
     def render(self):
-        print(self.current_prompt)
+        print(self.question)
+
+    def encode_question(self, question):
+        # Encode the question with the BERT tokenizer and model
+        inputs = self.encoder_tokenizer(question, return_tensors="pt", truncation=True, padding=True)
+        with torch.no_grad():
+            outputs = self.encoder_model(**inputs)
+        # Use the last hidden state as the question representation
+        return outputs.last_hidden_state[0, 0, :]
 
     def evaluate_prompt(self):
-        # Generate an answer from the GPT model for the current prompt
-        inputs = self.tokenizer.encode(self.current_prompt, return_tensors="pt")
-        outputs = self.model.generate(inputs, max_length=150, temperature=0.7)
-        generated_answer = self.tokenizer.decode(
-            outputs[:, inputs.shape[-1] :][0], skip_special_tokens=True
-        )
+        # TODO - maybe need to revisit this one
 
+        # Generate an answer from the BERT model for the current prompt
+        inputs = self.encoder_tokenizer(self.question, return_tensors="pt", truncation=True, padding=True)
+        with torch.no_grad():
+            outputs = self.encoder_model(**inputs)
+        generated_answer = self.encoder_tokenizer.decode(outputs[0], skip_special_tokens=True)
         # Compare the generated answer to the correct answer
-        if generated_answer == self.correct_answer:
+        if generated_answer == self.answer:
             return 1
         else:
             return -1
 
 
+class PolicyNet(nn.Module):
+    def __init__(self, state_size, action_size):
+        super(PolicyNet, self).__init__()
+        self.fc1 = nn.Linear(state_size, 128)
+        self.fc2 = nn.Linear(128, action_size)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.softmax(self.fc2(x), dim=-1)
+        return x
+
+
 class Agent:
-    """
-    def __init__(self, encoding_model_name):
-        self.tokenizer = AutoTokenizer.from_pretrained(encoding_model_name)
-        self.model = AutoModelForSeq2SeqLM.from_pretrained(encoding_model_name)
+    def __init__(self, state_size, action_size, lr=0.01, start_epsilon=1.0, end_epsilon=0.001, decay_steps=1e8):
+        self.policy_net = PolicyNet(state_size, action_size)
+        self.optimizer = torch.optim.Adam(self.policy_net.parameters(), lr=lr)
+        self.start_epsilon = start_epsilon
+        self.end_epsilon = end_epsilon
+        self.decay_steps = decay_steps
+        self.steps_done = 0
+
+    def get_epsilon(self):
+        fraction_of_steps = min(self.steps_done / self.decay_steps, 1)
+        return self.start_epsilon + fraction_of_steps * (self.end_epsilon - self.start_epsilon)
+
 
     def get_action(self, state):
-        # Given a state, select an action
-        # The action is selecting which question+answer to add to the prompt
-        pass
-        """
+        self.steps_done += 1
+        epsilon = self.get_epsilon()
+        state = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
+        action_probs = self.policy_net(state)
+        if np.random.rand() < epsilon:
+            action = torch.randint(high=action_size, size=(1,))  # Random action
+        else:
+            action = torch.argmax(action_probs)  # Best action
+        return action.item()
 
+    def train(self, env, num_episodes, episodes_per_update):
+        for episode in range(num_episodes):
+            all_states = []
+            all_actions = []
+            all_returns = []
 
-def train():
-    """
-    # Load the dataset
-    # For each episode:
-        # Get initial state (question)
-        # While not done:
-            # Agent chooses an action based on the current state
-            # Update the state based on the action
-            # If the action was the special action:
-                # Get the reward from the environment
-                # Update the agent based on the reward
-    pass
-    """
+            for _ in range(episodes_per_update):
+                states, actions, rewards = self.collect_episode(env)
+                returns = [sum(rewards[i:]) for i in range(len(rewards))]
+                all_states.extend(states)
+                all_actions.extend(actions)
+                all_returns.extend(returns)
 
+            self.optimizer.zero_grad()
 
-def evaluate():
-    # Similar to the train function, but without updating the agent
-    pass
+            for s, a, G in zip(all_states, all_actions, all_returns):
+                action_probs = self.policy_net(torch.tensor(s, dtype=torch.float32).unsqueeze(0))
+                log_prob = torch.log(action_probs[0, a])
+                loss = -log_prob * G
+                loss.backward()
 
+            self.optimizer.step()
 
-def collect_episode(env, policy_net):
-    """
-    states = []
-    actions = []
-    rewards = []
+    def collect_episode(self, env):
+        state = env.reset()
+        done = False
+        states = []
+        actions = []
+        rewards = []
 
-    s = env.reset()  # Reset environment to initial state
-    done = False
+        while not done:
+            action = self.get_action(state)
+            next_state, reward, done = env.step(action)
+            states.append(state)
+            actions.append(action)
+            rewards.append(reward)
+            state = next_state
 
-    while not done:
-        states.append(s)
+        return states, actions, rewards
 
-        action_probs = policy_net(torch.tensor(s, dtype=torch.float32))  # Get action probabilities from policy network
-        a = np.random.choice(len(action_probs), p=action_probs.detach().numpy())  # Sample action
-
-        actions.append(a)
-
-        s, r, done, _ = env.step(a)  # Take action in environment
-
-        rewards.append(r)
-
-    return states, actions, rewards
-    """
-
-
-# Usage:
-
-
-def reinforce():
-    """
-    # Assume these are lists that you've collected from an episode
-    states, actions, rewards = collect_episode(env, policy_net)
-
-    # Assume policy_net is your policy network
-    optimizer = torch.optim.Adam(policy_net.parameters())
-
-    optimizer.zero_grad()
-
-    for s_t, a_t, G_t in zip(states, actions, returns):
-        action_probs = policy_net(s_t)  # forward pass through the network
-        log_prob = torch.log(action_probs[a_t])  # log probability of the action taken
-        loss = -log_prob * G_t  # negative because we want to do gradient ascent
-        loss.backward()  # accumulate gradients
-
-    optimizer.step()  # perform a gradient ascent step
-    """
 
 
 if __name__ == "__main__":
@@ -169,14 +158,16 @@ if __name__ == "__main__":
     ]
     question, answer = dataset.iloc[0]
     env = Environment(
-        prompt=question,
-        correct_answer=answer,
-        gpt_model_name="gpt2",
-        special_action=0,
         training_dataset=dataset,
     )
-    a, b, c = env.step(1)
-    a, b, c = env.step(2)
-    a, b, c = env.step(3)
-    a_tag, b_tag, c_tag = env.step(0)
+
+    state_size = 768
+    action_size = 2290
+
+    num_episodes = 1000
+    episodes_per_update = 10
+
+    agent = Agent(state_size, action_size)
+    agent.train(env, num_episodes, episodes_per_update)
+
     print("stop")
---
+++ utils/utils.py
@@ -0,0 +1,23 @@
+import os
+from transformers import BertModel, BertTokenizer
+
+
+def load_or_download_model(model_name='bert-base-uncased', model_dir="./saved_model"):
+    model_dir = os.path.abspath(model_dir)
+
+    # Try loading the model from a local directory. If it doesn't exist, download it and save it locally
+    if (
+            os.path.exists(os.path.join(model_dir, "config.json"))
+            and os.path.exists(os.path.join(model_dir, "pytorch_model.bin"))
+            and os.path.exists(os.path.join(model_dir, "tokenizer_config.json"))
+            and os.path.exists(os.path.join(model_dir, "vocab.json"))
+    ):
+        tokenizer = BertTokenizer.from_pretrained(model_dir)
+        model = BertModel.from_pretrained(model_dir)
+    else:
+        tokenizer = BertTokenizer.from_pretrained(model_name)
+        model = BertModel.from_pretrained(model_name)
+        tokenizer.save_pretrained(model_dir)
+        model.save_pretrained(model_dir)
+
+    return tokenizer, model
