039688389c28f2602ee4dea40113c48c2e637285
danielmo <daniel.mor@doubleverify.com>
2024-01-29 20:59:05 +0200

    removed top_k_closest_questions_indices and retrieve use case

--- environment/env.py
+++ environment/env.py
@@ -13,11 +13,7 @@ class Environment:
         self.terminate_action = terminate_action
 
         # +1 for terminate action
-        self.action_space = (
-            self.retriever.top_k + 1
-            if hasattr(self.retriever, "top_k")
-            else len(dataset.train_data) + 1
-        )
+        self.action_space = len(dataset.train_data) + 1
 
         # Define observation space based on a sample observation
         # Half for generated prompt and half for storing the original question encodings
@@ -40,9 +36,8 @@ class Environment:
         if action == self.terminate_action:
             done = True
         else:
-            index_given_action = self.top_k_closest_questions_indices[action - 1]
             self.context_prompt = self.dataset.update_prompt(
-                index_given_action, self.context_prompt
+                action - 1, self.context_prompt
             )
             done = self.llm.is_prompt_too_long(
                 f"{self.dataset.prompt_prefix}{self.context_prompt}"
@@ -64,9 +59,6 @@ class Environment:
         self.question_encodings = self.retriever.encode(self.question)
         self.context_prompt = ""
         self.prompt_encodings = self.retriever.encode(self.context_prompt)
-        self.top_k_closest_questions_indices = self.retriever.retrieve(
-            encoding=self.question_encodings, mode=mode
-        )
         return np.concatenate([self.prompt_encodings, self.question_encodings])
 
     def evaluate_prompt(self):
